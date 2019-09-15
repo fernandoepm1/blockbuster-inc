a62297506d50a35c51415df28483bac3a45b4860
Fernando Eufrásio <fernando.epm1@gmail.com>
2019-09-15 20:47:42 -0300

    Change to bootstrap 4 and add the Lumen theme

--- BlockbusterInc/App_Start/RouteConfig.cs
+++ BlockbusterInc/App_Start/RouteConfig.cs
@@ -13,6 +13,17 @@ namespace BlockbusterInc
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
+            //Define routes in order of the most specific to most generic
+            //routes.MapRoute(
+            //    "MoviesByReleaseDate",
+            //    "movies/released/{year}/{month}",
+            //    new { controller = "Movies", action = "ByReleaseDate" },
+            //    //new { year = @"\d{4}", month = @"\d{2}" } // Forces the year to be 4 digit and month 2 digit in the URL
+            //    new { year = @"2015|2016", month = @"02|03" }
+            //);
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
